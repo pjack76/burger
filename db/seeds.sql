4a1d8a487cf4e17c542e3dcda963b62d3fceac52
Jack Long <putheara.long@gmail.com>
2020-05-03 20:58:17 -0400

    inital commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('quarter pounder', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('double cheeseburger',false);
