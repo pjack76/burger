2fe86cbf1f2ba662156c87b5f4a4f62d9d9af1c8
Jack Long <putheara.long@gmail.com>
2020-05-04 01:30:32 -0400

    fixed minor mistakes in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+#DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
@@ -5,5 +6,5 @@ USE burgers_db;
 CREATE TABLE burgers( 
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(32) NOT NULL,
-    devoured BOOLEAN,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id));
