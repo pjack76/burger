9bfb049fbbf0e7e8ed91ba1f81cfd4b5e63fabcd
Jack Long <putheara.long@gmail.com>
2020-05-06 23:45:43 -0400

    add minor change for heroku depoloyment

--- config/connection.js
+++ config/connection.js
@@ -10,6 +10,9 @@ var connection = mysql.createConnection({
 });
 
 // Make connection.
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -17,6 +20,6 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-
+};
 // Export connection for our ORM to use.
 module.exports = connection;
