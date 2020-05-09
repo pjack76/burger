f5c10e8cd2875ea034539c3ff24f6e4f5c34908c
Jack Long <putheara.long@gmail.com>
2020-05-09 09:41:06 -0400

    update config file

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,11 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -10,9 +14,7 @@ var connection = mysql.createConnection({
 });
 
 // Make connection.
-if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
